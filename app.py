3334471fd690d5f1675ca98a3624bfeec20e9503
Ghislain Lao <82372832+gislainlao@users.noreply.github.com>
2022-09-11 05:15:00 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.title('BOSS')
+st.write('''
+# App Simple pour la prévision des fleurs d'Iris
+Cette application prédit la catégorie des fleurs d'Iris
+''')
+
+st.sidebar.header("Les parametres d'entrée")
+
+
