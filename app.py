fcc721045e2c945d8ffd90e4195be93aebcd180d
Ghislain Lao <82372832+gislainlao@users.noreply.github.com>
2022-09-11 05:23:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
+impote datetime
 
 
-st.title('BOSS')
-st.write('''
-# App Simple pour la prévision des fleurs d'Iris
-Cette application prédit la catégorie des fleurs d'Iris
-''')
+datetime.datetime.today()
+datetime.datetime(2018, 2, 13, 22, 25, 0, 693541)
+dj= datetime.datetime.today().strftime("%d/%m/%Y")
 
-st.sidebar.header("Les parametres d'entrée")
+c=['DECES-INVALIDITE TOTALE ET DEFINITIVE','DECES-INVALIDITE TOTALE ET DEFINITIVE ET PERTE EMPLOI']
 
 
+    #Title display
+html_temp = """
+<div style="background-color: tomato; padding:10px; border-radius:10px">
+<h1 style="color: white; text-align:center">PLATEFORM DE SIMULATION DE CREDIT</h1>
+</div>
+<p style="font-size: 20px; font-weight: bold; text-align:center">Calcul de la prime…</p>
+"""
+st.markdown(html_temp, unsafe_allow_html=True)
